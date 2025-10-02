a9dbb30c89a4be6defb4df853e5ea1a0ffda1ad0
athia26 <widas22.athia@gmail.com>
2025-10-02 14:05:25 +0700

    user2 menambahkan baris

--- Test.java
+++ Test.java
@@ -1,5 +1,6 @@
 public class Test{
     public static void main(String[] args){
         System.out.println("Hi, ini baris pertama");
+        System.out.println("User 2 menambahkan file baru");
     }
 }
